0364c8cd8408be868fcdf78c52711bab029da4b6
itlovel <lovelypanjaitans@gmail.com>
2026-03-17 19:25:57 +0700

    feat: add API BookController and BookResource"

---
+++ app/Http/Controllers/Api/BookController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Book;
+use App\Http\Resources\BookResource;
+use Illuminate\Http\Request;
+
+class BookController extends Controller
+{
+    public function index()
+    {
+        return BookResource::collection(Book::paginate(10));
+    }
+
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'title' => 'required|string|max:255',
+            'author' => 'required|string|max:255',
+            'publisher' => 'nullable|string|max:255',
+            'year' => 'nullable|integer',
+        ]);
+
+        $book = Book::create($data);
+        return new BookResource($book);
+    }
+
+    public function show(Book $book)
+    {
+        return new BookResource($book);
+    }
+
+    public function update(Request $request, Book $book)
+    {
+        $data = $request->validate([
+            'title' => 'sometimes|required|string|max:255',
+            'author' => 'sometimes|required|string|max:255',
+            'publisher' => 'nullable|string|max:255',
+            'year' => 'nullable|integer',
+        ]);
+
+        $book->update($data);
+        return new BookResource($book);
+    }
+
+    public function destroy(Book $book)
+    {
+        $book->delete();
+        return response()->noContent();
+    }
+}
---
+++ app/Http/Resources/BookResource.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class BookResource extends JsonResource
+{
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'author' => $this->author,
+            'publisher' => $this->publisher,
+            'year' => $this->year,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
