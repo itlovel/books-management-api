e49677af6f4c3ee15c57e9dd45ec1ea20f73f046
itlovel <lovelypanjaitans@gmail.com>
2026-04-01 13:45:30 +0700

    feat: wrap registration and login routes in auth middleware

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,14 @@
 use App\Http\Controllers\Api\BookController;
 use App\Http\Controllers\Api\AuthController;
 
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
+Route::middleware('auth:api')->group(function () {
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::get('/me', [AuthController::class, 'me']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+   
+    Route::apiResource('/products', ProductController::class);
+    
+    Route::apiResource('books', BookController::class);
 
-Route::apiResource('books', BookController::class);
+});
