41234573b6564f6de4fdb9fb1e9e1a7aea0504a9
itlovel <lovelypanjaitans@gmail.com>
2026-04-01 13:37:03 +0700

    feat: add registration and login routes to AuthController

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,9 @@
 <?php
 
 use App\Http\Controllers\Api\BookController;
+use App\Http\Controllers\Api\AuthController;
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 
 Route::apiResource('books', BookController::class);
