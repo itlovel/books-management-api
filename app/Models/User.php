057dfac2af272c70287c87395b6a475266e10e05
itlovel <lovelypanjaitans@gmail.com>
2026-04-03 23:37:22 +0700

    refactor: reorganize JWT methods in User model and update route definitions

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,16 +11,6 @@ use Tymon\JWTAuth\Contracts\JWTSubject;
 
 class User extends Authenticatable implements JWTSubject
 {
-    public function getJWTIdentifier()
-    {
-        return $this->getKey();
-    }
-
-    public function getJWTCustomClaims()
-    {
-        return [];
-    }
-
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
 
@@ -56,5 +46,15 @@ class User extends Authenticatable implements JWTSubject
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+    
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,10 @@
 use App\Http\Controllers\Api\BookController;
 use App\Http\Controllers\Api\AuthController;
 
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
 Route::middleware('auth:api')->group(function () {
-    Route::post('/register', [AuthController::class, 'register']);
-    Route::post('/login', [AuthController::class, 'login']);
     Route::get('/me', [AuthController::class, 'me']);
     Route::post('/logout', [AuthController::class, 'logout']);
    
